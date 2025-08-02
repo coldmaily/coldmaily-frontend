689826e1ac8c33a12c477b3ac8f614da8a2c1f35
SAHIL VERMA <111533506+itz-Sahil-Verma@users.noreply.github.com>
2025-08-03 02:01:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'], // Google profile picture domain
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async rewrites() {
     return [
       {
