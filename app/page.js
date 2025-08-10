9419a3238abf83b468e6d603f69d5aca5ad0193d
Sahil <sahil@e2enetworks.com>
2025-08-10 14:46:05 +0530

    new changes

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+"use client"; 
 import Header from "@/components/LandingPage/Header";
 import Hero from "@/components/LandingPage/Hero";
 import Footer from "@/components/LandingPage/Footer";
 
 export default function Home() {
+
   return (
     <main className="flex flex-col min-h-screen">
       <Header />
